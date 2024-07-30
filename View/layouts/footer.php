6f74caef65597d5653dbc947b213eccc3030ce40
Amanda093 <vovigadagamer@gmail.com>
2024-07-29 21:23:59 -0300

    aplicando mvc

---
+++ View/layouts/footer.php
@@ -0,0 +1,55 @@
+<footer class="footer">
+  <div class="container container__footer">
+    <div class="section__footer section-logo__footer">
+      <a href="index.html" class="logo-wrapper__footer"
+        ><img src="img/Outros/inclusipet.png" alt="Logo Inclusipet" class="logo__footer"
+      /></a>
+    </div>
+    <div class="section__footer section__footer-links">
+      <ul class="menu__footer">
+        <li>
+          <a href="pages/QuemSomos/quemsomos.html" class="links__footer a-under">Quem Somos</a>
+        </li>
+        <li>
+          <a href="pages/Unidades/unidades.html" class="links__footer a-under">Unidades</a>
+        </li>
+        <li>
+          <a href="pages/Blog/blog.html" class="links__footer a-under">Blog</a>
+        </li>
+        <li>
+          <a href="pages/Adocao/adocao.html" class="links__footer a-under">Adoção</a>
+        </li>
+        <li>
+          <a href="pages/Contato/contato.html" class="links__footer a-under">Contato</a>
+        </li>
+      </ul>
+    </div>
+    <div class="section__footer">
+      <ul class="menu__footer">
+        <li>
+          <a href="https://www.instagram.com/inclusipet/" class="links__footer"
+            ><img src="../../img/Contato/instagram.png" alt="Instagram" class="rede-social"
+          /></a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/InclusiPet23?t=sUSZebJOD0wUFlcThrDaJw&s=09"
+            class="links__footer"
+            ><img src="../../img/Contato/twitter.png" alt="twitter" class="rede-social"
+          /></a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/profile.php?id=61553182305622&mibextid=ZbWKwL"
+            class="links__footer"
+            ><img src="../../img/Contato/facebook.png" alt="Facebook" class="rede-social"
+          /></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="container container__footer">
+    <div class="hr-footer"></div>
+  </div>
+  <div class="container container__footer">®2023 Inclusipet. Todos os direitos reservados.</div>
+</footer>
---
+++ View/layouts/header.php
@@ -0,0 +1,67 @@
+<header class="header">
+  <div class="container container__header">
+    <a href="index.html">
+      <img src="img/Outros/inclusipet.png" alt="Logo Inclusipet" class="logo__header" />
+    </a>
+    <ul class="menu__header">
+      <li>
+        <a href="pages/QuemSomos/quemsomos.html" class="links__header a-under">Quem Somos</a>
+      </li>
+      <li>
+        <a href="pages/Unidades/unidades.html" class="links__header a-under">Unidades</a>
+      </li>
+      <li>
+        <a href="pages/Blog/blog.html" class="links__header a-under">Blog</a>
+      </li>
+      <li>
+        <a href="pages/Adocao/adocao.html" class="links__header a-under">Adoção</a>
+      </li>
+      <li>
+        <a href="pages/Contato/contato.html" class="links__header a-under">Contato</a>
+      </li>
+      <li>
+        <a href="pages/Login/login.html"
+          ><img src="img/Login/login.png" alt="Login" class="login__header"
+        /></a>
+      </li>
+    </ul>
+    <button class="barra__header">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+        />
+      </svg>
+    </button>
+    <div class="dropdown__header">
+      <li>
+        <a href="pages/QuemSomos/quemsomos.html" class="links__header">Quem Somos</a>
+      </li>
+      <li>
+        <a href="pages/Unidades/unidades.html" class="links__header">Unidades</a>
+      </li>
+      <li>
+        <a href="pages/Blog/blog.html" class="links__header">Blog</a>
+      </li>
+      <li>
+        <a href="pages/Adocao/adocao.html" class="links__header">Adoção</a>
+      </li>
+      <li>
+        <a href="pages/Contato/contato.html" class="links__header">Contato</a>
+      </li>
+      <li>
+        <button class="botao-solido-branco" onclick="location.href='pages/Login/login.html'" type="button">
+          Login
+        </button>
+      </li>
+</div>
+  </div>
+</header>
+<script src="js/dropdown_header.js"></script>
