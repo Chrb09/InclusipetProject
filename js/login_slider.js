5c5f578ceb5816b897b13ff29e1c0e3a4a36df2a
Chrb09 <carloshrbarile@gmail.com>
2024-08-14 11:05:49 -0300

    ajutes gerais responsividade

--- js/login_slider.js
+++ js/login_slider.js
@@ -1,6 +1,7 @@
 const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
 const containerlogin = document.getElementById("container-login");
+const signUpForm = document.getElementById("sign-up-form");
 
 signUpButton.addEventListener("click", () => {
   containerlogin.classList.add("right-panel-active");
@@ -9,3 +10,11 @@ signUpButton.addEventListener("click", () => {
 signInButton.addEventListener("click", () => {
   containerlogin.classList.remove("right-panel-active");
 });
+
+function cadastrarnext() {
+  signUpForm.classList.toggle("next");
+}
+
+$("#sign-up-cpf").mask("000.000.000-00");
+$("#sign-up-cep").mask("00000-000");
+$("#sign-up-tel").mask("(00)00000-0000");
