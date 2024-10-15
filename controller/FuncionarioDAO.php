0eb02b6d20471a3ef6b42cf337f66dd8c4b6c77a
BernardoVxexra <bernardo.santos13@etec.sp.gov.br>
2024-10-14 21:43:06 -0300

    Ajustando

--- controller/FuncionarioDAO.php
+++ controller/FuncionarioDAO.php
@@ -2,5 +2,66 @@
 //Iniciando a estutura DAO do funcionário
 require_once('../controller/FuncionarioDAO.php'); //Chamando DAO
 
+class FuncionarioDAO implements FuncionarioDAOInterface{
+    private $conn;
+    private $url;
 
+    public function __construct(PDO $conn, $url) {
+      $this->conn = $conn;
+      $this->url = $url;
+    }
+
+    public function buildfuncionario($data)
+    {
+        $funcionario = new Funcionario(); //Chamando a classe Funcionario
+
+        $funcionario->CodFuncionario = $data["codfuncionario"];
+        $funcionario->CodCargo = $data["codcargo"];
+        $funcionario->Senha = $data["senha"];
+        $funcionario->Nome = $data["nome"];
+        $funcionario->RG = $data["rg"];
+        $funcionario->CPF = $data["cpf"];
+        $funcionario->Telefone = $data["telefone"];
+        $funcionario->CEP = $data["cep"];
+        $funcionario->CodUnidade = $data["codunidade"];
+        $funcionario->Token = $data["token"];
+        $funcionario->Imagem = $data["imagem"];
+
+        return $funcionario;
+    }
+
+    public function create(Funcionario $funcionario, $authfuncionario = false) {
+      
+    }
+
+    public function update(Funcionario $funcionario, $redirect = true) {
+
+    }
+
+    public function verifyToken($protected = false) {
+
+    }
+
+    public function setTokenToSession($token, $redirect = true) {
+
+    }
+    
+
+    public function findById($codcliente) {
+
+    }
+
+    public function findByToken($token) {
+
+    }
+
+    public function destroyToken() {
+
+    }
+
+    public function changePassword(Funcionario $funcionario) {
+      
+    }
+
+}
 ?>
--- model/Classes/Funcionario.php
+++ model/Classes/Funcionario.php
@@ -12,6 +12,30 @@ class Funcionario {
   public $Token;
   public $Imagem ;
 
+  public function getFullName($funcionario) {
+    return $funcionario->nome;
+  }
+
+  public function generateToken() {
+    return bin2hex(random_bytes(50));
+  }
   
+  public function generatePassword($Senha) {
+    return password_hash($Senha, PASSWORD_DEFAULT);
+  }
+
+  public function imageGenerateName() {
+    return bin2hex(random_bytes(60)) . ".jpg";
+  }
 }
+  interface FuncionarioDAOInterface{
+      public function buildfuncionario($data);
+      public function create(Funcionario $funcionario , $authfuncionario = false );
+      public function update(Funcionario $funcionario , $redirect = true );
+      public function verifyToken($protected = false);
+
+      
+  }
+
+
 ?>
