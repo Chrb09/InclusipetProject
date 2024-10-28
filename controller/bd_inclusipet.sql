55a3b79b034f3a5e2df666d626750ac4be862575
Chrb09 <carloshrbarile@gmail.com>
2024-10-28 11:59:23 -0300

    listagem das unidades

--- controller/bd_inclusipet.sql
+++ controller/bd_inclusipet.sql
@@ -212,9 +212,10 @@ CREATE TABLE `unidade` (
   `Bairro` varchar(50) NOT NULL,
   `Telefone` varchar(15) NOT NULL,
   `HorarioInicial` time DEFAULT NULL,
-  `HorarioFinal` time DEFAULT NULL
+  `HorarioFinal` time DEFAULT NULL,
+  `Link` TEXT NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-INSERT INTO `unidade` (`CodUnidade`, `Nome`, `Endereco`, `Bairro`, `Telefone`, `HorarioInicial`, `HorarioFinal`) VALUES (1, 'Inclusipet - Guarulhos', 'R. Conceição da Feira', 'Jardim pres. dutra - SP', '(11) 11111-1111', '08:00:00', '21:00:00'), (2, 'Inclusipet - Vila Cisper', 'R. Cícero Dantas', 'Vila Cisper - SP', '(11) 22222-2222', '08:00:00', '18:00:00'), (3, 'Inclusipet - Artur Alvim', 'Av. Líder', 'Artur Alvim - SP', '(11) 33333-3333', '10:00:00', '18:00:00');
+INSERT INTO `unidade` (`CodUnidade`, `Nome`, `Endereco`, `Bairro`, `Telefone`, `HorarioInicial`, `HorarioFinal`, `Link`) VALUES (1, 'Inclusipet - Guarulhos', 'R. Conceição da Feira', 'Jardim pres. dutra - SP', '(11) 11111-1111', '08:00:00', '21:00:00', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.9433340295095!2d-46.433593725612525!3d-23.426414056646507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce89f1b8bcf965%3A0xff24a059d917838b!2sR.%20Concei%C3%A7%C3%A3o%20da%20Feira%20-%20Jardim%20Pres.%20Dutra%2C%20Guarulhos%20-%20SP%2C%2007173-010!5e0!3m2!1spt-BR!2sbr!4v1697764260412!5m2!1spt-BR!2sbr'), (2, 'Inclusipet - Vila Cisper', 'R. Cícero Dantas', 'Vila Cisper - SP', '(11) 22222-2222', '08:00:00', '18:00:00', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.9811661599797!2d-46.497951325609456!3d-23.497187859235876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce605e5a1481e3%3A0xb0cd6094d2133515!2sR.%20C%C3%ADcero%20Dantas%20-%20Ermelino%20Matarazzo%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2003818-130!5e0!3m2!1spt-BR!2sbr!4v1697773935746!5m2!1spt-BR!2sbr'), (3, 'Inclusipet - Artur Alvim', 'Av. Líder', 'Artur Alvim - SP', '(11) 33333-3333', '10:00:00', '18:00:00', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.369311320792!2d-46.47061682560695!3d-23.555176061363234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce66e1dbe1105b%3A0xb388139f5d5c5750!2sAv.%20L%C3%ADder%20-%20Artur%20Alvim%2C%20S%C3%A3o%20Paulo%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1697774090354!5m2!1spt-BR!2sbr');
 -- --------------------------------------------------------
 
 --
--- view/pages/Unidades/unidades.php
+++ view/pages/Unidades/unidades.php
@@ -19,64 +19,37 @@
   <?php
   $activePage = "unidades";
   include('../../components/headers/header.php');
-  ?>
+
+  require_once("../../../controller/DAO/AgendamentoDAO/AgendamentoDAO.php");
+  $agendamentoDao = new AgendamentoDAO($conn, $BASE_URL);
+
+  $unidades = $agendamentoDao->getAllUnidade()
+    ?>
   <!--Conteudo-->
   <div class="container container__unidades">
     <div class="container">
       <div class="titulo">Unidades</div>
       <div class="hr"></div>
     </div>
-    <div class="cartao__unidades cartao1">
-      <div class="cartao-body">
-        <strong class="cartao-titulo">Inclusipet - Guarulhos</strong>
-        <div class="cartao-subtitulo">
-          <div class="status"></div>
-          <div class="texto-subtitulo">Aberto das 8h às 21h</div>
+    <?php foreach ($unidades as $unidade): ?>
+      <div class="cartao__unidades">
+        <div class="cartao-body">
+          <strong class="cartao-titulo"><?= $unidade[1] ?></strong>
+          <div class="cartao-subtitulo">
+            <div class="status"></div>
+            <div class="texto-subtitulo">Aberto das <?= explode(':00', trim($unidade[5]))[0] ?>h às
+              <?= explode(':00', trim($unidade[6]))[0] ?>h
+            </div>
+          </div>
+          <p class="card-text">
+            <?= $unidade[2] ?><br />
+            <?= $unidade[3] ?><br />
+            <?= $unidade[4] ?>
+          </p>
         </div>
-        <p class="card-text">
-          R. Conceição da Feira<br />
-          Jardim pres. dutra - SP<br />
-          (11) 11111-1111
-        </p>
+        <iframe src="<?= $unidade[7] ?>" class="iframe"></iframe>
       </div>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.9433340295095!2d-46.433593725612525!3d-23.426414056646507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce89f1b8bcf965%3A0xff24a059d917838b!2sR.%20Concei%C3%A7%C3%A3o%20da%20Feira%20-%20Jardim%20Pres.%20Dutra%2C%20Guarulhos%20-%20SP%2C%2007173-010!5e0!3m2!1spt-BR!2sbr!4v1697764260412!5m2!1spt-BR!2sbr"
-        class="iframe"></iframe>
-    </div>
-    <div class="cartao__unidades cartao2">
-      <div class="cartao-body">
-        <strong class="cartao-titulo">Inclusipet - Vila Cisper</strong>
-        <div class="cartao-subtitulo">
-          <div class="status"></div>
-          <div class="texto-subtitulo">Aberto das 8h às 18h</div>
-        </div>
-        <p class="card-text">
-          R. Cícero Dantas<br />
-          Vila Cisper - SP<br />
-          (11) 22222-2222
-        </p>
-      </div>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.9811661599797!2d-46.497951325609456!3d-23.497187859235876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce605e5a1481e3%3A0xb0cd6094d2133515!2sR.%20C%C3%ADcero%20Dantas%20-%20Ermelino%20Matarazzo%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2003818-130!5e0!3m2!1spt-BR!2sbr!4v1697773935746!5m2!1spt-BR!2sbr"
-        class="iframe"></iframe>
-    </div>
-    <div class="cartao__unidades cartao3">
-      <div class="cartao-body">
-        <strong class="cartao-titulo">Inclusipet - Artur Alvim</strong>
-        <div class="cartao-subtitulo">
-          <div class="status"></div>
-          <div class="texto-subtitulo">Aberto das 10h as 18h</div>
-        </div>
-        <p class="card-text">
-          Av. Líder<br />
-          Artur Alvim - SP<br />
-          (11) 33333-3333
-        </p>
-      </div>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.369311320792!2d-46.47061682560695!3d-23.555176061363234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce66e1dbe1105b%3A0xb388139f5d5c5750!2sAv.%20L%C3%ADder%20-%20Artur%20Alvim%2C%20S%C3%A3o%20Paulo%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1697774090354!5m2!1spt-BR!2sbr"
-        class="iframe"></iframe>
-    </div>
+    <?php endforeach; ?>
   </div>
   <!-- FOOTER -->
   <?php
@@ -84,6 +57,7 @@
   ?>
 </body>
 <script>
+  /*
   const cartao1 = document.querySelector(".cartao1");
   const cartao2 = document.querySelector(".cartao2");
   const cartao3 = document.querySelector(".cartao3");
